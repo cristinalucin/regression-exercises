b6fe2316336493048654f4b328709acf9c6bc3d7
Cristina Lucin <cristinajlucin@gmail.com>
2022-11-07 12:31:08 -0600

    created scaling file

---
+++ model.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+import numpy as np
+import scipy.stats as stats
+
+import env
+
+from sklearn.model_selection import train_test_split
+
+def train_validate_test_split(df, target, seed=123):
+    '''
+    This function takes in a dataframe, the name of the target variable
+    (for stratification purposes), and an integer for a setting a seed
+    and splits the data into train, validate and test. 
+    Test is 15% of the original dataset, validate is .1765*.85= 15% of the 
+    original dataset, and train is 75% of the original dataset. 
+    The function returns, in this order, train, validate and test dataframes. 
+    '''
+    train_validate, test = train_test_split(df, test_size=0.15, 
+                                            random_state=seed, 
+                                            stratify=df[target])
+    train, validate = train_test_split(train_validate, test_size=0.1765, 
+                                       random_state=seed,
+                                       stratify=train_validate[target])
+    return train, validate, test
--- wrangle.py
+++ wrangle.py
@@ -1,8 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+import os
 import numpy as np
 import env
+
 from env import user, password, host
 
 def get_zillow_data():
@@ -26,10 +29,14 @@ def get_zillow_data():
         df = pd.read_sql(query, get_connection('zillow'))
 
         # Write that dataframe to disk for later. Called "caching" the data for later.
-        df.to_csv(filename)
+        df.to_csv(filename, index=False)
 
         # Return the dataframe to the calling code
         return df
+    
+def get_connection(db, user=env.user, host=env.host, password=env.password):
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
     
 def remove_outliers(df,feature_list):
     ''' utilizes IQR to remove data which lies beyond 
@@ -46,6 +53,23 @@ def remove_outliers(df,feature_list):
         lower_limit = Q1 - 1.5 * IQR
         #remove outliers
         df = df[(df[feature] > lower_limit) & (df[feature] < upper_limit)]
+    
+    return df
+    
+def clean_zillow(df):
+    ''' This function takes in zillow data and returns a clean dataset.
+    '''
+    # renaming columns
+    df = df.rename(columns = {'bedroomcnt':'bedrooms', 
+                          'bathroomcnt':'bathrooms', 
+                          'calculatedfinishedsquarefeet':'area',
+                          'taxvaluedollarcnt':'tax_value', 
+                          'yearbuilt':'year_built',
+                          'taxamount':'tax_amount'
+                    })
+    
+    # Replace a whitespace sequence or empty with a NaN value and reassign this manipulation to df. 
+    df = df.replace(r'^\s*$', np.nan, regex=True)
     
     return df
     
